e08bbf0805b65af05a9a99eadb2b4e9d6c3cf5e6
gino king <gino_king@oright.com.tw>
2021-02-22 18:04:06 +0800

    上課

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>testing</h1>
+    <main id="home">
+        <div class="card">
+            <p class="title">Title</p>
+            <div class="description">
+                <p>des</p>
+                <p>des</p>
+                <p>des</p>
+            </div>
+        </div>
+        <br>
+        <p>outside</p>
+    </main>
 @endsection
--- resources/views/pb.blade.php
+++ resources/views/pb.blade.php
@@ -1,6 +1,16 @@
-<?php
+@extends('layouts.app')
 
-
-
-
-	echo "pb";
+@section('content')
+    <main id="pb">
+        <div class="card">
+            <p class="title">Title</p>
+            <div class="description">
+                <p>des</p>
+                <p>des</p>
+                <p>des</p>
+            </div>
+        </div>
+        <br>
+        <p>outside</p>
+    </main>
+@endsection
