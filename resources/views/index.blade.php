ceafa410e2797520c28c03fcfe9cc534b22a7b6f
金沅禹 <king@jinyuanyudeMacBook-Pro.local>
2021-06-27 10:43:53 +0800

    index 整理

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -73,56 +73,30 @@
 					</div>
 				</div>
 			</section>
-			<!--=============================================================================
-				/Front Section
-			===============================================================================-->
 		
 			<!--=============================================================================
 				About Section
 			===============================================================================-->
 			<section id='about' class='about-section section' >
-				
 				<div class='basic-info section-block' >
-					
 					<div class='container' >
-					
 						<div class='section-header text-center' >
-							
 							<h2>Basic Info About Me</h2>
-							
 							<!--divider-->
 							<div class='divider-draft center' ></div>
-							<!--/divider-->
-							
-							
 						</div>
-						
 						<div class='row' >
-							
 							<div class='col-md-4' >
 								<div class='about-person-img' >
-									
 									<img src='img/man2.png' alt='symp'>
-									
 								</div>
-							
 							</div>
-							
-							
 							<div class='col-md-8 about-info' >
-								
-								
-								
-								
 								<p>
 									Lorem ipsum dolor sit amet, in quodsi vulputate pro. Ius illum vocent mediocritatem an, cum dicta iriure at. Ubique maluisset vel te, his dico vituperata ut. Pro ei phaedrum maluisset. Ex audire suavitate has, ei quodsi tacimates sapientem sed, pri zril ubique ut. Te cum tation munere noluisse. Enim torquatos ne pri, eum mollis salutandi corrumpit et, fugit apeirian duo ad. Ad oportere voluptatibus nec, sea ei civibus praesent. At timeam expetenda inciderint has. Invidunt constituto ne per, paulo omnium quaerendum his eu, cu congue consul cetero cum. Solet honestatis et eum. Lorem ipsum dolor sit amet, in quodsi vulputate pro. Ius illum vocent mediocritatem an, cum dicta iriure at. Ubique maluisset vel te, his dico vituperata ut. Pro ei phaedrum maluisset. Ex audire suavitate has, ei quodsi tacimates sapientem sed, pri zril ubique ut. Te cum tation munere noluisse. Enim torquatos ne pri, eum mollis salutandi corrumpit et, fugit apeirian duo ad.
 								</p>
-								
 								<div class='clearfix' ></div>
-								
-								
 								<ul class='info-list' >
-									
 									<li>
 										<div class='inner' >
 											<h4>Name</h4>
@@ -147,117 +121,67 @@
 											<p>Dokri</p>
 										</div>
 									</li>
-									
-									
-									
 								</ul>
-								
-								<a href='#' class='symp-btn' >Hire Me For Work</a>
-								
 								<a href='#' class='symp-btn link-btn' ><i class='ion-ios-download' ></i>Download Resume</a>
-								
 							</div>
-						
-							
-						
 						</div>
-						
-						
-						
-						
 					</div>
-					
-					
 				</div>
-				
 				<div class='about-icons section-block' >
-					
 					<div class='container' >
-						
 						<div class='row' >
-							
 							<div class='col-md-3' >
 								<div class='service' >
-									
 									<div class='icon' >
 										<i class='ion-ios-lightbulb' ></i>
 									</div>
-									
 									<div class='content' >
-										
 										<h4>Creative</h4>
-										
 										<p>
 											Ut aboramus, numquam corpora qui naex. Dolore aliquando id duo, voluptua laboramus quo ad.
 										</p>
-										
 									</div>
-									
 								</div>
 							</div>
-							
 							<div class='col-md-3' >
 								<div class='service' >
-									
 									<div class='icon' >
 										<i class='ion-trophy' ></i>
 									</div>
-									
 									<div class='content' >
-										
 										<h4>Winner</h4>
-										
 										<p>
 											Ut aboramus, numquam corpora qui naex. Dolore aliquando id duo, voluptua laboramus quo ad.
 										</p>
-										
 									</div>
-									
 								</div>
 							</div>
-							
 							<div class='col-md-3' >
 								<div class='service' >
-									
 									<div class='icon' >
 										<i class='ion-ios-glasses' ></i>
 									</div>
-									
 									<div class='content' >
-										
 										<h4>Smart</h4>
-										
 										<p>
 											Ut aboramus, numquam corpora qui naex. Dolore aliquando id duo, voluptua laboramus quo ad.
 										</p>
-										
 									</div>
-									
 								</div>
 							</div>
-							
 							<div class='col-md-3' >
 								<div class='service' >
-									
 									<div class='icon' >
 										<i class='ion-ios-bolt' ></i>
 									</div>
-									
 									<div class='content' >
-										
 										<h4>Powerful</h4>
-										
 										<p>
 											Ut aboramus, numquam corpora qui naex. Dolore aliquando id duo, voluptua laboramus quo ad.
 										</p>
-										
 									</div>
-									
 								</div>
 							</div>
-							
-							
-							
 							<div class='col-md-3' >
 								<div class='service' >
 									
@@ -277,194 +201,126 @@
 									
 								</div>
 							</div>
-							
 							<div class='col-md-3' >
 								<div class='service' >
-									
 									<div class='icon' >
 										<i class='ion-happy' ></i>
 									</div>
-									
 									<div class='content' >
-										
 										<h4>Intelligent</h4>
-										
 										<p>
 											Ut aboramus, numquam corpora qui naex. Dolore aliquando id duo, voluptua laboramus quo ad.
 										</p>
-										
 									</div>
-									
 								</div>
 							</div>
-							
 							<div class='col-md-3' >
 								<div class='service' >
-									
 									<div class='icon' >
 										<i class='ion-ios-game-controller-b' ></i>
 									</div>
-									
 									<div class='content' >
-										
 										<h4>Gamer</h4>
-										
 										<p>
 											Ut aboramus, numquam corpora qui naex. Dolore aliquando id duo, voluptua laboramus quo ad.
 										</p>
-										
 									</div>
-									
 								</div>
 							</div>
-							
 							<div class='col-md-3' >
 								<div class='service' >
-									
 									<div class='icon' >
 										<i class='ion-ios-pulse-strong' ></i>
 									</div>
-									
 									<div class='content' >
-										
 										<h4>Healthy</h4>
-										
 										<p>
 											Ut aboramus, numquam corpora qui naex. Dolore aliquando id duo, voluptua laboramus quo ad.
 										</p>
-										
 									</div>
-									
 								</div>
 							</div>
-							
-							
-							
-							
 						</div>
-						
-						
 					</div>
 				</div>
-				
 				<div class='funfacts-block section-block' data-stellar-background-ratio='.2' >
-					
 					<div class='container' >
-						
 						<div class='row' >
-							
 							<div class='col-md-3 col-sm-6 funfact' >
 								<div class='content' >
 									<h4>243</h4>
 									<p>Awards Won</p>
 								</div>
 							</div>
-							
 							<div class='col-md-3 col-sm-6 funfact' >
 								<div class='content' >
 									<h4>845</h4>
 									<p>Happy Customers</p>
 								</div>
 							</div>
-							
 							<div class='col-md-3 col-sm-6 funfact' >
 								<div class='content' >
 									<h4>938</h4>
 									<p>Projects Done</p>
 								</div>
 							</div>
-							
 							<div class='col-md-3 col-sm-6 funfact' >
 								<div class='content' >
 									<h4>342</h4>
 									<p>Games Played</p>
 								</div>
 							</div>
-							
 						</div>
-						
 					</div>
-					
 				</div>
-				
 				<div class='testimonials-block section-block' >
 					<div class='container' >
-					
 						<div class='section-header text-center' >
-							
 							<h2 class='animate text-over-block' >Testimonials</h2>
-							
-							
 							<!--divider-->
 							<div class='divider-draft center' ></div>
-							<!--/divider-->
-							
-							
 						</div>
-						
-						<div class='row' >
-							
+						<div class='row justify-content-center' >
 							<div class='col-md-8 col-md-offset-2' >
-								
 								<div class='testimonials-slider' >
-									
 									<div class='testimonial' >
 										<p>
-											
 											Ad optere volibus nec, sea ei civibus prasent. At timeam expetenda inciderint has. Invidunt constituto ne per, paulo omnium queum his eu, cu congue consul cetero. Invidunt constituto ne per, paulo omnium his eu, cu congue consul cetero.
-										
 										</p>
-										
 										<div class='author' >
 											<h4>Jack Smith</h4>
 											<p>CEO at Google</p>
 										</div>
 									</div>
-									
 									<div class='testimonial' >
 										<p>
 											Ad oportere voluptatibus nec, sea ei civibus praesent. At timeam expetenda inciderint has. Invidunt constituto ne per, paulo omnium quaerendum his eu, cu congue consul cetero. Invidunt constituto ne per, paulo omnium quaerendum his eu, cu congue consul cetero.
 										</p>
-										
 										<div class='author' >
 											<h4>Jessica Smith</h4>
 											<p>CEO at Google</p>
 										</div>
 									</div>
-									
 									<div class='testimonial' >
 										<p>
 											Ad oportere voluptatibus nec, sea ei civibus praesent. At timeam expetenda inciderint has. Invidunt constituto ne per, paulo omnium quaerendum his eu, cu congue consul cetero. Invidunt constituto ne per, paulo omnium quaerendum his eu, cu congue consul cetero.
 										</p>
-										
 										<div class='author' >
 											<h4>Jim Smith</h4>
 											<p>CEO at Google</p>
 										</div>
 									</div>
-									
-									
-									
-									
 								</div>
-								
 							</div>
-							
 						</div>
-					
 					</div>
 				</div>
-				
 				<div class='footer bg-lightgray section-block' >
-					
 					<div class='container' >
-						
 						<div class='row' >
-							
 							<div class='col-xs-6 text-left' >
 								<h4>Johny Doe</h4>
 							</div>
-							
 							<div class='col-xs-6 text-right' >
 								<ul class='footer-social' >
 									<li>
@@ -489,19 +345,10 @@
 									</li>
 								</ul>
 							</div>
-							
 						</div>
-						
-						
 					</div>
-					
 				</div>
-				
-				
 			</section>
-			<!--=============================================================================
-				/About Section
-			===============================================================================-->
 			
 			<!--=============================================================================
 				Resume Section
