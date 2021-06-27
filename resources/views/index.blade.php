2fa8cdc9f208b0724cf092f9e244c3da88813e50
金沅禹 <king@jinyuanyudeMacBook-Pro.local>
2021-06-27 13:11:42 +0800

    稍微改點內容，還沒改完

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,17 +32,16 @@
 					<div class='front-person-img' >
 						<!--person's image-->
 						<img src='/images/man1.png' alt='Symp' >
-						<!--/person's image-->
 					</div>
 					<!--person's titles-->
 					<div class='front-person-titles' >
 						<!--title1-->
 						<span class='t1' >
-							Designer
+							Engineer
 						</span>
 						<!--title2-->
 						<span class='t2' >
-							Gamer guy
+							Photographer
 						</span>
 						<!--title3-->
 						<span class='t3' >
@@ -68,7 +67,7 @@
 					</nav>
 					<div class='front-heading text-center' >
 						<h2>
-							Johny Doe
+							Gino King
 						</h2>
 					</div>
 				</div>
@@ -100,7 +99,7 @@
 									<li>
 										<div class='inner' >
 											<h4>Name</h4>
-											<p>Johny Doe</p>
+											<p>金沅禹</p>
 										</div>
 									</li>
 									<li>
@@ -118,7 +117,7 @@
 									<li>
 										<div class='inner' >
 											<h4>Hometown</h4>
-											<p>Dokri</p>
+											<p>桃園中壢</p>
 										</div>
 									</li>
 								</ul>
@@ -369,133 +368,56 @@
 					<div class='container' >
 						<ul class='timeline' >
 							<li class='timeline-header' >
-								<h4>Education</h4>
-							</li>
-							<li>
-								<div class='timeline-desc' >
-									<h4>2005-2007</h4>
-								</div>
-								<div class='timeline-content' >
-									<h4>Masters Degree</h4>
-									<span>@CSS College Larkana</span>
-									<p>
-										Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat vituperata suscipiantur et, et fabellas explicari adipiscing quo.
-									</p>
-								</div>
-							</li>
-							
-							<li class='inverse' >
-								
-								<div class='timeline-desc' >
-									
-									<h4>2005-2007</h4>
-									
-								</div>
-								
-								<div class='timeline-content' >
-									
-									<h4>Masters Degree</h4>
-									<span>@CSS College Larkana</span>
-									
-									<p>
-										Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat vituperata suscipiantur et, et fabellas explicari adipiscing quo.
-									</p>
-									
-								</div>
-								
-							</li>
-							
-							<li>
-								
-								<div class='timeline-desc' >
-									
-									<h4>2005-2007</h4>
-									
-								</div>
-								
-								<div class='timeline-content' >
-									
-									<h4>Masters Degree</h4>
-									<span>@CSS College Larkana</span>
-									
-									<p>
-										Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat vituperata suscipiantur et, et fabellas explicari adipiscing quo.
-									</p>
-									
-								</div>
-								
-							</li>
-							
-							
-							<li class='timeline-header' >
 								<h4>Experience</h4>
 							</li>
-							
-							
 							<li>
-								
 								<div class='timeline-desc' >
-									
-									<h4>2005-2007</h4>
-									
+									<h4>2015-2019</h4>
 								</div>
-								
 								<div class='timeline-content' >
-									
-									<h4>Web Designer</h4>
-									<span>@Themeforest</span>
-									
+									<h4>中原大學資訊管理系</h4>
+									<span>@CYCU Department of Information Management</span>
 									<p>
 										Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat vituperata suscipiantur et, et fabellas explicari adipiscing quo.
 									</p>
-									
 								</div>
-								
 							</li>
-							
 							<li class='inverse' >
-								
 								<div class='timeline-desc' >
-									
-									<h4>2005-2007</h4>
-									
+									<h4>2019-2020</h4>
 								</div>
-								
 								<div class='timeline-content' >
-									
-									<h4>Web Developer</h4>
-									<span>@CodeCanyon</span>
-									
+									<h4>趣比比股份有限公司-實習生</h4>
+									<span>@Tripbnb.com</span>
 									<p>
 										Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat vituperata suscipiantur et, et fabellas explicari adipiscing quo.
 									</p>
-									
 								</div>
-								
 							</li>
-							
 							<li>
-								
 								<div class='timeline-desc' >
-									
-									<h4>2005-2007</h4>
-									
+									<h4>2020/06-2020/12</h4>
 								</div>
-								
 								<div class='timeline-content' >
-									
-									<h4>Blogger</h4>
-									<span>@blogspot</span>
-									
+									<h4>趣比比股份有限公司-全端工程師</h4>
+									<span>@Tripbnb.com</span>
 									<p>
 										Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat vituperata suscipiantur et, et fabellas explicari adipiscing quo.
 									</p>
-									
 								</div>
-								
 							</li>
-							
-							
+							<li class='inverse' >
+								<div class='timeline-desc' >
+									<h4>2021/01-至今</h4>
+								</div>
+								<div class='timeline-content' >
+									<h4>歐萊德國際股份有限公司-後端工程師</h4>
+									<span>@Tripbnb.com</span>
+									<p>
+										Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat vituperata suscipiantur et, et fabellas explicari adipiscing quo.
+									</p>
+								</div>
+							</li>
 						</ul>
 					</div>
 				</div>
@@ -507,222 +429,136 @@
 							<h2 class='animate text-over-block' >My Hobbies</h2>
 							<!--divider-->
 							<div class='divider-draft center' ></div>
-							<!--/divider-->
 						</div>
-						
 						<div class='row' >
-							
 							<div class='col-md-2 col-sm-4 col-xs-6' >
-								
 								<div class='hobby' >
-									
 									<div class='icon' >
 										<i class='ion-ios-game-controller-b' ></i>
 									</div>
-									
 									<h4>Gaming</h4>
-									
 								</div>
-								
 							</div>
-							
 							<div class='col-md-2 col-sm-4 col-xs-6' >
-								
 								<div class='hobby' >
-									
 									<div class='icon' >
 										<i class='ion-ios-book' ></i>
 									</div>
-									
 									<h4>Reading</h4>
-									
 								</div>
-								
 							</div>
-							
 							<div class='col-md-2 col-sm-4 col-xs-6' >
-								
 								<div class='hobby' >
-									
 									<div class='icon' >
 										<i class='ion-ios-chatboxes' ></i>
 									</div>
-									
 									<h4>Chat</h4>
-									
 								</div>
-								
 							</div>
-							
 							<div class='col-md-2 col-sm-4 col-xs-6' >
-								
 								<div class='hobby' >
-									
 									<div class='icon' >
 										<i class='ion-ios-musical-notes' ></i>
 									</div>
-									
 									<h4>Music</h4>
-									
 								</div>
-								
 							</div>
-							
 							<div class='col-md-2 col-sm-4 col-xs-6' >
-								
 								<div class='hobby' >
-									
 									<div class='icon' >
 										<i class='ion-beer' ></i>
 									</div>
-									
 									<h4>Drinking</h4>
-									
 								</div>
-								
 							</div>
-							
 							<div class='col-md-2 col-sm-4 col-xs-6' >
-								
 								<div class='hobby' >
-									
 									<div class='icon' >
 										<i class='ion-ios-football' ></i>
 									</div>
-									
 									<h4>Football</h4>
-									
 								</div>
-								
 							</div>
-							
-							
-							
 						</div>
-						
-						
-						
 					</div>
 				</div>
-				
 				<div class='skills-block section-block' >
-					
 					<div class='container' >
-					
 						<div class='section-header text-center' >
 							<h2 class='animate text-over-block' >My Skills</h2>
-							
 							<!--divider-->
 							<div class='divider-draft center' ></div>
-							<!--/divider-->
-							
 						</div>
-						
-						
 						<div class='row' >
-							
 							<div class='col-md-6' >
-								
 								<div class='skill' >
-									
 									<h4>HTML/CSS</h4>
-									
 									<div class='skill-bar' data-percent='90' >
 										<div class='bar' >
 											<div class='percent' >90%</div>
 										</div>
 									</div>
-									
 								</div>
-								
 								<div class='skill' >
-									
 									<h4>php</h4>
-									
-									<div class='skill-bar' data-percent='70' >
+									<div class='skill-bar' data-percent='80' >
 										<div class='bar' >
-											<div class='percent' >70%</div>
+											<div class='percent' >80%</div>
 										</div>
 									</div>
-									
 								</div>
-								
 								<div class='skill' >
-									
-									<h4>HTML/CSS</h4>
-									
+									<h4>Bootstrap</h4>
 									<div class='skill-bar' data-percent='90' >
 										<div class='bar' >
 											<div class='percent' >90%</div>
 										</div>
 									</div>
-									
 								</div>
-								
-								
-								
+								<div class='skill' >
+									<h4>Python</h4>
+									<div class='skill-bar' data-percent='60' >
+										<div class='bar' >
+											<div class='percent' >60%</div>
+										</div>
+									</div>
+								</div>
 							</div>
-							
 							<div class='col-md-6' >
-								
 								<div class='skill' >
-									
-									<h4>VB</h4>
-									
-									<div class='skill-bar' data-percent='90' >
+									<h4>JavaScript</h4>
+									<div class='skill-bar' data-percent='80' >
 										<div class='bar' >
-											<div class='percent' >90%</div>
+											<div class='percent' >80%</div>
 										</div>
 									</div>
-									
 								</div>
-								
 								<div class='skill' >
-									
-									<h4>Wordpress</h4>
-									
-									<div class='skill-bar' data-percent='85' >
+									<h4>CodeIgniter</h4>
+									<div class='skill-bar' data-percent='75' >
 										<div class='bar' >
-											<div class='percent' >85%</div>
+											<div class='percent' >75%</div>
 										</div>
 									</div>
-									
 								</div>
-								
 								<div class='skill' >
-									
-									<h4>SEO</h4>
-									
-									<div class='skill-bar' data-percent='95' >
+									<h4>Laravel</h4>
+									<div class='skill-bar' data-percent='40' >
 										<div class='bar' >
-											<div class='percent' >95%</div>
+											<div class='percent' >40%</div>
 										</div>
 									</div>
-									
 								</div>
-								
-								
-								
 							</div>
-							
-							
-							
 						</div>
-						
 					</div>
-					
 				</div>
-				
 				<div class='footer bg-lightgray section-block' >
-					
 					<div class='container' >
-						
 						<div class='row' >
-							
 							<div class='col-xs-6 text-left' >
 								<h4>Johny Doe</h4>
 							</div>
-							
 							<div class='col-xs-6 text-right' >
 								<ul class='footer-social' >
 									<li>
@@ -747,21 +583,10 @@
 									</li>
 								</ul>
 							</div>
-							
 						</div>
-						
-						
 					</div>
-					
 				</div>
-				
-				
 			</section>
-			<!--=============================================================================
-				/Resume Section
-			===============================================================================-->
-			
-			
 			<!--=============================================================================
 				Services Section
 			===============================================================================-->
@@ -1317,10 +1142,6 @@
 				
 				
 			</section>
-			<!--=============================================================================
-				/Services Section
-			===============================================================================-->
-			
 			
 			<!--=============================================================================
 				Portfolio Section
@@ -2062,7 +1883,7 @@
 				<span></span>
 			</div>
 		</div>
-		
+
 		@include('layouts.js')
 	</body>
 </html>
